bafc28c14c08bb0b736564974f7c6eaef830d57b
chaker ramdani <limio.viviok@gmail.com>
2024-03-04 11:29:40 +0100

    first commit

--- src/contexts/QuizContext.jsx
+++ src/contexts/QuizContext.jsx
@@ -68,7 +68,7 @@ function QuizProvider({ children }) {
   useEffect(function () {
     async function getData() {
       try {
-        const res = await fetch("../src/data/data.json");
+        const res = await fetch("../../public/data.json");
         const data = await res.json();
         dispatch({ type: "dataReceived", payload: data });
       } catch (err) {
